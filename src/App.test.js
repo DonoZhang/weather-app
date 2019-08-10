cc4f3686961fc682df79ea7b2d7e6d41b50c6a98
Dapeng Zhang <dapengzhang1992@gmail.com>
2019-08-10 15:36:41 +1000

    First commit - add testing for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import { findByTestAttr } from './Utilities/testingUtilities'
+import './setupTests';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const setUp = (props = {}) =>{
+  const component = shallow(<App {...props} />);
+  return component;
+}
+
+describe('Header Component', ()=>{
+  let component;
+  beforeEach(() => {
+      component = setUp();
+  });
+
+  it('Should render without errors', ()=>{
+      const wrapper = findByTestAttr(component, 'headerComponent');
+      expect(wrapper.length).toBe(1);
+  });
+
+  it('Should render a logo', ()=>{
+      const logo = findByTestAttr(component, 'logoIMG');
+      expect(logo.length).toBe(1);
+  });
 });
