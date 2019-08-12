b94e15d5ff6a04616c2fdde00a4c6085de07eaeb
Dapeng Zhang <dapengzhang1992@gmail.com>
2019-08-12 19:21:44 +1000

    Add simulate event function test for SharedButton

--- src/components/button/index.test.js
+++ src/components/button/index.test.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme'
 import { findByTestAttr, checkProps } from '../../../Utilities/utilities';
 import SharedButton from './index';
-import { italic } from 'ansi-colors';
+import { call } from 'redux-saga/effects';
 
 describe('SharedButton Component', ()=>{
     describe('Checking Property Types', ()=>{
         it('Should NOT throw a warning', ()=>{
             const expectedProps = {
-                buttonText: 'Example Button Text',
-                emitEvent: ()=>{
-
+                    buttonText: 'Example Button Text',
+                    emitEvent: ()=>{
                 }
             };
             const propsError = checkProps(SharedButton, expectedProps);
@@ -20,12 +19,12 @@ describe('SharedButton Component', ()=>{
     
     describe('Renders', ()=>{
         let wrapper;
+        let mockFunction;
         beforeEach(()=>{
+            mockFunction = jest.fn();
             const props = {
                 buttonText: 'Example Button Text',
-                emitEvent: ()=>{
-
-                }
+                emitEvent: mockFunction
             };
             wrapper = shallow(<SharedButton {...props}/>);
         });
@@ -33,6 +32,13 @@ describe('SharedButton Component', ()=>{
         it('Should render a button', ()=>{
             const button = findByTestAttr(wrapper, 'buttonComponent');
             expect(button.length).toBe(1);
+        });
+
+        it('Should emit callback function on click event', ()=>{
+            const button = findByTestAttr(wrapper, 'buttonComponent');
+            button.simulate('click');
+            const callback = mockFunction.mock.calls.length;
+            expect(callback).toBe(1);
         });
     });
 });
