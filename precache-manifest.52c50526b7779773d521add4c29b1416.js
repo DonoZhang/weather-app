55ee74311760bc4ac8797a9ed36e260304645f84
Dapeng Zhang <dapengzhang1992@gmail.com>
2019-08-16 17:35:36 +1000

    Updates

---
+++ precache-manifest.52c50526b7779773d521add4c29b1416.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac8f067450c9f3e9f06a2f3e2cfe2ed",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "690c550ae23ca074c0a1",
+    "url": "/weather-app/static/css/main.cddf6fd2.chunk.css"
+  },
+  {
+    "revision": "a57c861260989229efaf",
+    "url": "/weather-app/static/js/2.33af7a8c.chunk.js"
+  },
+  {
+    "revision": "690c550ae23ca074c0a1",
+    "url": "/weather-app/static/js/main.70053bcb.chunk.js"
+  },
+  {
+    "revision": "292c0f51470a512f9bc6",
+    "url": "/weather-app/static/js/runtime~main.661dbc95.js"
+  },
+  {
+    "revision": "0b135705c8d35475843d62bdb2155efe",
+    "url": "/weather-app/static/media/App-Background.0b135705.jpg"
+  },
+  {
+    "revision": "768b228e0d0ea7cf343e1dedfb4ddd6e",
+    "url": "/weather-app/static/media/loading.768b228e.gif"
+  }
+]);
